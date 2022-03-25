1f784a39bd78c344421aa44c4515b95d6f76f3f4
MateuszKost <matekos1604@gmail.com>
2022-03-26 00:04:30 +0100

    Created Entities and CommonObjects

---
+++ CommonObjects/ShipType.cs
@@ -0,0 +1,11 @@
+﻿namespace CommonObjects
+{
+    public enum ShipType : int
+    {
+        Carrier = 5,
+        Battleship = 4,
+        Destroyer = 3,
+        Submarine = 3,
+        PatrolBoat = 2
+    }
+}
---
+++ Entities/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace Entities
+{
+    /*
+     * Player - class for storing information about player
+     * 
+     * NickName - name of the player
+     * Ships - the list of ships assigned to the player
+     */
+    public class Player
+    {
+        public string NickName { get; set; }
+        public List<Ship> Ships { get; set; }
+
+        private Player(string nickName, List<Ship> ships)
+        {
+            NickName = nickName;
+            Ships = ships;
+        }
+
+        public static Player CreatePlayer(string nickName, List<Ship> ships)
+        {
+            return new Player(nickName, ships);
+        }
+    }
+}
