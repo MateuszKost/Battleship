d91cdd8a15420a7796fc2aef5dc6fc774a9d7b12
MateuszKost <matekos1604@gmail.com>
2022-03-26 13:04:33 +0100

    Small corrections

--- CommonObjects/CommonVariables.cs
+++ CommonObjects/CommonVariables.cs
@@ -4,8 +4,8 @@
     {
         public const int DefaultMapSize = 100;
 
-        public static readonly int Length = 10;
-        public static readonly IEnumerable<int> X_Axis = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        public static readonly IEnumerable<char> Y_Axis = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+        public static readonly int DeafultBoardLength = 10;
+        public static readonly IEnumerable<int> DefaultXAxis = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        public static readonly IEnumerable<char> DefaultYAxis = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
     }
 }
