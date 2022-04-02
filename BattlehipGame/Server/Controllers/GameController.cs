17fc320f4eb54c338d8f82655b78e06c73ec5423
MateuszKost <matekos1604@gmail.com>
2022-04-02 11:22:32 +0200

    GetPlayers endpoint created

---
+++ BattlehipGame/Server/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using Battleship;
+using MainObjects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BattlehipGame.Server.Controllers
+{
+    public class GameController : Controller
+    {
+        [HttpGet("GetPlayers")]
+        public ActionResult<Player[]> GetPlayers()
+        {
+            CreationAlgorithm creationAlgorithm = new CreationAlgorithm();
+            Player firstPlayer =  creationAlgorithm.CreatePlayer("First player");
+            Player secondPlayer =  creationAlgorithm.CreatePlayer("Second player");
+
+            return new Player[] { firstPlayer, secondPlayer };
+        }
+    }
+}
--- Battleship/Game.cs
+++ Battleship/Game.cs
@@ -7,10 +7,8 @@ namespace Battleship
         internal static void Start()
         {
             CreationAlgorithm creationAlgorithm = new CreationAlgorithm();
-            //Player playerOne = Player.CreatePlayer("First player");
             Player playerOne = creationAlgorithm.CreatePlayer("First player");
             Console.WriteLine("Created {0}", playerOne.NickName);
-            //Player playerTwo = Player.CreatePlayer("Second player");
             Player playerTwo = creationAlgorithm.CreatePlayer("Second player");
             Console.WriteLine("Created {0}", playerTwo.NickName);
             Console.WriteLine();
