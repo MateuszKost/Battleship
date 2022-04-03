31ba71da18c41bba60e76e95380c8dd3651971ca
MateuszKost <matekos1604@gmail.com>
2022-04-03 19:56:11 +0200

    Frontend works properly

--- BattlehipGame/Server/Controllers/GameController.cs
+++ BattlehipGame/Server/Controllers/GameController.cs
@@ -1,16 +1,21 @@
 ﻿using Battleship;
 using MainObjects;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using ViewModels;
 
 namespace BattlehipGame.Server.Controllers
 {
     public class GameController : Controller
     {
-        private Dictionary<Player, bool> _temp = new Dictionary<Player, bool>();
-
         private Player _firstPlayer;
         private Player _secondPlayer;
+        private IMemoryCache _cache;
+
+        public GameController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
 
         [HttpGet("GetPlayers")]
         public PlayerViewModel[] GetPlayers()
@@ -19,8 +24,8 @@ namespace BattlehipGame.Server.Controllers
             _firstPlayer =  creationAlgorithm.CreatePlayer("First player");
             _secondPlayer =  creationAlgorithm.CreatePlayer("Second player");
 
-            _temp.Add(_firstPlayer, true);
-            _temp.Add(_secondPlayer, true);
+            _cache.Set("firstPlayer", _firstPlayer);
+            _cache.Set("secondPlayer", _secondPlayer);
 
             return new PlayerViewModel[] { new PlayerViewModel(_firstPlayer.NickName, _firstPlayer.OwnMap), new PlayerViewModel(_secondPlayer.NickName, _secondPlayer.OwnMap) };
         }
@@ -28,18 +33,10 @@ namespace BattlehipGame.Server.Controllers
         [HttpGet("GetSimulationList")]
         public IEnumerable<ShootViewModel> GetSimulationList()
         {
-            CreationAlgorithm creationAlgorithm = new CreationAlgorithm();
-            _firstPlayer = creationAlgorithm.CreatePlayer("First player");
-            _secondPlayer = creationAlgorithm.CreatePlayer("Second player");
+            _firstPlayer = _cache.Get<Player>("firstPlayer");
+            _secondPlayer = _cache.Get<Player>("secondPlayer");
             //Check later ship creating, some points of ship sometimes are not in line
-            Console.WriteLine("Created {0}", _firstPlayer.NickName);
-            Console.WriteLine("Created {0}", _secondPlayer.NickName);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Starting the simulation...");
-
             SimulationAlgorithm simulation = new SimulationAlgorithm();
-
             return simulation.Start(_firstPlayer, _secondPlayer);
         }
     }
